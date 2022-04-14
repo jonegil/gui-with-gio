2f1fc71848edee34c692240121c7b29651d47b57
Jon Egil Strand <jon.egil.strand@gmail.com>
2022-04-14 16:00:44 +0200

    Revert "Resolve issues with the simplest two chapters."
    This reverts commit ab8b393a7da4da4b9fe84fcb96129290b1cbd235.

--- egg_timer/code/07_progressbar/main.go
+++ egg_timer/code/07_progressbar/main.go
@@ -65,7 +65,7 @@ func draw(w *app.Window) error {
 		// listen for events in the window.
 		case e := <-w.Events():
 
-			// detect which type of event
+			// detect what type of event
 			switch e := e.(type) {
 
 			// this is sent when the application should re-render.
