82fd148af417fa163ace8a64332a47f12ca285e0
Jon Egil Strand <jon.egil.strand@gmail.com>
2022-11-06 11:38:22 +0100

    Missed this one, doh.

--- teleprompter/code/main.go
+++ teleprompter/code/main.go
@@ -283,11 +283,11 @@ func draw(w *app.Window) error {
 				ScrollBounds: image.Rectangle{
 					Min: image.Point{
 						X: 0,
-						Y: -200,
+						Y: -int(unit.Dp(200)),
 					},
 					Max: image.Point{
 						X: 0,
-						Y: 200,
+						Y: int(unit.Dp(200)),
 					},
 				},
 			}.Add(gtx.Ops)
