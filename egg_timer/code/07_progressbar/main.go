bdbb7010a92e53a45f48d28057a477504ccf7fbe
zill_laiss <153055114+zilllaiss@users.noreply.github.com>
2024-02-24 20:14:17 +0700

    changed channel to bool

--- egg_timer/code/07_progressbar/main.go
+++ egg_timer/code/07_progressbar/main.go
@@ -14,16 +14,16 @@ import (
 )
 
 // Define the progress variables, a channel and a variable
-var progress float32
 var progressIncrementer chan float32
+var progress float32
 
 func main() {
 	// Setup a separate channel to provide ticks to increment progress
-	progressIncrementer = make(chan float32)
+	progressIncrementer = make(chan bool)
 	go func() {
 		for {
 			time.Sleep(time.Second / 25)
-			progressIncrementer <- 0.004
+			progressIncrementer <- true
 		}
 	}()
 
